c507942ecfdf00ea5118b6715ae2917af1c84e6d
pkijania <139795239+pkijania@users.noreply.github.com>
2024-08-20 11:00:23 +0200

    Update wpd-plugin-calculator.php

--- wpd-plugin-calculator.php
+++ wpd-plugin-calculator.php
@@ -1,8 +1,8 @@
 <?php
 /*
-Plugin Name: Power calculator
-Description: Simple calculator and correspondence plugin
-Version: 1.9
+Plugin Name: Heat pumps power calculator
+Description: Simple calculator and correspondence plugin for heat pumps
+Version: 2.0
 Author: Przemysław Kijania
 Author URI: https://przemyslawkijania.pl/
 */
@@ -25,7 +25,7 @@ class Calculator
     // Get all the pump models from csv file
     private function get_pump_models()
     {
-        $csvFile = WP_PLUGIN_DIR . '/power-calculator/Spis_pomp.csv';
+        $csvFile = WP_PLUGIN_DIR . '/heat_pumps_power_calculator/heat_pumps_list.csv';
         $pumps = [];
         $index = 1;
         if (($handle = fopen($csvFile, 'r')) !== FALSE) {
@@ -86,63 +86,60 @@ class Calculator
             $results = array_filter($pumps_models, function ($pumps_models) use ($search_pumps) {
                 return count(array_intersect_assoc($search_pumps, $pumps_models)) == count($search_pumps);
             });
-            return $results;
+            $new_results = array_values($results);
+            return $new_results;
         }
     }
 
-    // Send an e-mail both to the admin and to the user that consists of all the provided information, the estimated power needed to heat the building and information about the selected heat pump
-    public function deliver_mail($power, $area, $standard, $name_of_pump, $id, $efficiency, $price, $link)
+    // Assign all the data to variables
+    public function assign_variable($power, $area, $standard, $pump)
     {
-        if (isset($_POST['cf-submitted'])) {
-            $name = sanitize_text_field($_POST["cf-name"]);
-            $email = sanitize_email($_POST["cf-email"]);
-            $subject = sanitize_text_field($_POST["cf-subject"]);
-            $message = esc_textarea($_POST["cf-message"]);
-            
-            $variables = array();
-            $variables['user'] = sanitize_text_field($name);
-            $variables['area'] = sanitize_text_field($area);
-            $variables['standard'] = sanitize_text_field($standard);
-            $variables['power'] = sanitize_text_field($power);
-            $variables['name'] = sanitize_text_field($name_of_pump);
-            $variables['id'] = sanitize_text_field($id);
-            $variables['efficiency'] = sanitize_text_field($efficiency);
-            $variables['price'] = sanitize_text_field($price);
-            $variables['link'] = esc_url($link);
-            $variables['cf-message'] = sanitize_text_field($message);
+        $variables = array();
+    
+        $variables['user'] = sanitize_text_field($_POST["cf-name"]);
+        $variables['email'] = sanitize_email($_POST["cf-email"]);
+        $variables['subject'] = sanitize_text_field($_POST["cf-subject"]);
+        $variables['cf-message'] = esc_textarea($_POST["cf-message"]);
+        $variables['area'] = sanitize_text_field($area);
+        $variables['standard'] = sanitize_text_field($standard);
+        $variables['power'] = sanitize_text_field($power);
+    
+        foreach ($pump as $row) {
+            $variables['name'] = sanitize_text_field($row['Nazwa']);
+            $variables['id'] = sanitize_text_field($row['Id']);
+            $variables['efficiency'] = sanitize_text_field($row['Moc']);
+            $variables['price'] = sanitize_text_field($row['Cena [PLN]']);
+            $variables['link'] = esc_url($row['Link']);
+        }
+        return $variables;
+    }
 
-            $template_path = WP_PLUGIN_DIR . '/power-calculator/template.html';
-            if (!file_exists($template_path)) {
-                echo 'Plik szablonu nie został znaleziony: ' . $template_path;
-            }
-            $template = file_get_contents($template_path);
+    // Send an e-mail both to the admin and to the user that consists of all the provided information
+    public function send_mail($variables)
+    {
+        $template_path = WP_PLUGIN_DIR . '/heat_pumps_power_calculator/heat_pumps_template.html';
+        if (!file_exists($template_path)) {
+            echo 'Plik szablonu nie został znaleziony: ' . $template_path;
+            return;
+        }
 
-            foreach ($variables as $key => $value) {
-                $template = str_replace('{{' . $key . '}}', $value, $template);
-            }
+        $template = file_get_contents($template_path);
 
-            if ($power <= 15 || !empty($name_of_pump)){
-                $message .= "\nWyniki dla wyceny pompy ciepła:";
-                $message .= "\nDla powierzchni ogrzewania: " . $area . " m2 oraz standardu wykonania: " . $standard . " kWh/m2 szacowana ilość mocy potrzebna do ogrzania domu to: " . $power . " kW";
-                $message .= "\nSzczegóły dotyczące wybranej pompy ciepła:";
-                $message .= "\nNazwa: " . $name_of_pump;
-                $message .= "\nId: " . $id;
-                $message .= "\nMoc: " . $efficiency . " kW";
-                $message .= "\nCena: " . $price . " zł";
-                $message .= "\nLink do strony producenta: ". $link;
-                $message .= "\nWszelkie informacje do korespondencji znajdują się pod podanym linkiem: https://sevro.pl/kontakt/";
+        foreach ($variables as $key => $value) {
+            $template = str_replace('{{' . $key . '}}', $value, $template);
+        }
 
-                $subject = "Wycena pompy ciepła";
-                $to = get_option('admin_email');
-                $headers = "From: $name <$email>" . "\r\n";
-                $headers .= "CC: $email" . "\r\n";
-                $headers .= "Content-Type: text/html; charset=UTF-8";
-        
-                if (wp_mail($to, $subject, $template, $headers)) {
-                    echo '<div>';
-                    echo "<h4>Wyniki zostały wysłane na podany e-mail.</h4>";
-                    echo '</div>';
-                }
+        if ($variables['power'] <= 16 || !empty($variables['name'])){
+            $subject = "Wycena pompy ciepła";
+            $to = get_option('admin_email');
+            $headers = "From: " . $variables['user'] . " <" . $variables['email'] . ">" . "\r\n";
+            $headers .= "CC: " . $variables['email'] . "\r\n";
+            $headers .= "Content-Type: text/html; charset=UTF-8";
+
+            if (wp_mail($to, $subject, $template, $headers)) {
+                echo '<div>';
+                echo "<h4>Poniższe wyniki zostały wysłane na podany w formularzu email:</h4>";
+                echo '</div>';
             }
         }
     }
@@ -164,6 +161,7 @@ function html_calculation_code()
         echo 'Dom budowany obecnie - od 70 do 90 <br/>';
         echo 'Dom energooszczędny - od 60 do 70 <br/>';
         echo 'Dom pasywny - 15 <br/>';
+        echo '<br>';
         echo '</p>';
 
         echo '<h4>';
@@ -180,6 +178,7 @@ function html_calculation_code()
         echo 'Standard wykonania [kWh/m2 rok] <br/>';
         echo '<input type="range" name="cf-standard" min="5" max="200" value="' . (isset($_POST["cf-standard"]) ? esc_attr($_POST["cf-standard"]) : '5') . '" size="40" oninput="this.nextElementSibling.value = this.value" />';
         echo '<output>' . (isset($_POST["cf-standard"]) ? esc_attr($_POST["cf-standard"]) : '5') . '</output>';
+        echo '<br>';
         echo '</p>';
 
         echo '<p><input type="submit" name="cf-count" value="Oblicz"></p>';
@@ -188,7 +187,7 @@ function html_calculation_code()
 }
 
 // Show the second formula - input fields for information about the client
-function html_form_code($power = null, $area = null, $standard = null, $name_of_pump = null, $id = null, $efficiency = null, $price = null, $link = null)
+function html_form_code($power = null, $area = null, $standard = null)
 {
     if (isset($_POST['cf-count']) && !isset($_POST['cf-result']) && !isset($_POST['cf-submitted'])) {
         echo '<form action="' . esc_url($_SERVER['REQUEST_URI']) . '" method="post" onsubmit="return validateForm()">';
@@ -211,45 +210,88 @@ function html_form_code($power = null, $area = null, $standard = null, $name_of_
 
         echo '<p>';
         echo 'Dodatkowe informacje <br/>';
-        echo '<textarea rows="10" cols="100%" name="cf-message">' . (isset($_POST["cf-message"]) ? esc_attr($_POST["cf-message"]) : '') . '</textarea>';
+        echo '<textarea rows="5" cols="118" name="cf-message">' . (isset($_POST["cf-message"]) ? esc_attr($_POST["cf-message"]) : '') . '</textarea>';
         echo '<span class="error" style="color:red;display:none">To pole nie może być puste</span>';
         echo '</p>';
 
         echo '<input type="hidden" name="cf-power" value="' . esc_attr($power) . '" />';
         echo '<input type="hidden" name="cf-area" value="' . esc_attr($area) . '" />';
         echo '<input type="hidden" name="cf-standard" value="' . esc_attr($standard) . '" />';
-        echo '<input type="hidden" name="cf-div-name" value="' . esc_attr($name_of_pump) . '" />';
-        echo '<input type="hidden" name="cf-div-id" value="' . esc_attr($id) . '" />';
-        echo '<input type="hidden" name="cf-div-efficieny" value="' . esc_attr($efficiency) . '" />';
-        echo '<input type="hidden" name="cf-div-price" value="' . esc_attr($price) . '" />';
-        echo '<input type="hidden" name="cf-div-link" value="' . esc_attr($link) . '" />';
 
-        echo '<p><input type="submit" name="cf-submitted" value="Wyślij"></p>';
+        echo '<p><br><input type="submit" name="cf-submitted" value="Wyślij"></p>';
         echo '</form>';
     }
 }
 
 // Show the third formula - the estimated power needed to heat the building and information about the selected heat pump
-function html_results_code($power, $area, $standard, $name_of_pump, $id, $efficiency, $price, $link)
+function html_results_code($power, $area, $standard, $pumps)
 {
     if (isset($_POST['cf-submitted']) && !isset($_POST['cf-result'])) {
         echo '<form action="' . esc_url($_SERVER['REQUEST_URI']) . '" method="post">';
-        echo 'Dla powierzchni ogrzewania: ' . esc_html($area) . ' m2 oraz standardu wykonania: ' . esc_html($standard) . ' kWh/m2 szacowana ilość mocy potrzebna do ogrzania domu to: ' . esc_html($power) . ' kW<br>';
-        echo 'Szczegóły dotyczące wybranej pompy ciepła:<br>';
+        echo '<div style="color: black;">';
 
-        if ($power > 15 || empty($name_of_pump)) {
-            echo 'Nie znaleziono odpowiedniej pompy ciepła<br>';
-            echo 'W celu doboru urządzenia prosimy o bezpośredni kontakt lub o wprowadzenie innych danych<br>';
-        }
-        else {
+        echo '<p>';
+        echo 'Dla powierzchni ogrzewania: <strong>' . esc_html($area) . ' m2</strong> oraz standardu wykonania: <strong>' . esc_html($standard) . ' kWh/m2</strong>,<br>';
+        echo 'szacowana ilość mocy potrzebna do ogrzania domu to około: <strong>' . esc_html($power) . ' kW</strong><br>';
+        echo '</p>';
+
+        echo '<h4>';
+        echo '<br>';
+        echo 'Szczegóły dotyczące wybranej pompy ciepła:<br>';
+        echo '</h4>';
+
+        if ($power > 16 || empty($pumps)) {
             echo '<p>';
-            echo 'Nazwa: ' . esc_attr($name_of_pump) . '<br>';
-            echo 'Numer id: ' . esc_attr($id) . '<br>';
-            echo 'Moc: ' . esc_attr($efficiency) . ' kW <br>';
-            echo 'Cena: ' . esc_attr($price) . ' zł <br>';
-            echo 'Strona producenta: ' . esc_attr($link) . '<br>';
+            echo 'Nie znaleziono odpowiedniego kotła pelletowego<br>';
+            echo 'W celu doboru urządzenia prosimy o bezpośredni kontakt lub o wprowadzenie innych danych<br>';
+            echo '</p>';
+        } else {
+            foreach ($pumps as $pump) {
+                echo '<p>';
+                echo 'Nazwa: <strong>' . esc_attr($pump["Nazwa"]) . '</strong><br>';
+                echo 'Numer id: <strong>' . esc_attr($pump["Id"]) . '</strong><br>';
+                echo 'Moc: <strong>' . esc_attr($pump["Moc"]) . ' kW </strong><br>';
+                echo 'Cena od: <strong>' . esc_attr($pump["Cena [PLN]"]) . ' zł brutto (8% VAT) </strong><br>';
+                echo 'Strona producenta: <strong>' . esc_attr($pump["Link"]) . '</strong><br>';
+                echo '</p>';
+            }
+
+            echo '<h5>';
+            echo '<br>';
+            echo 'Kalkulacja Zawiera:<br>';
+            echo '</h5>';
+
+            echo '<p style="font-size:10px; line-height: 2;">';
+            echo '- Dostarczenie materiałów niezbędnych do instalacji<br>';
+            echo '- Montaż wspornika gruntowego<br>';
+            echo '- Montaż jednostki zewnętrznej<br>';
+            echo '- Wykonanie niezbędnych otworów w ścianach<br>';
+            echo '- Przeprowadzenie instalacji freonowej (pompy split)<br>';
+            echo '- Montaż jednostki wewnętrznej<br>';
+            echo '- Połączenie jednostek, wykonanie próby szczelności<br>';
+            echo '- Montaż zbiornika ciepłej wody użytkowej wraz z niezbędną armaturą i zabezpieczeniami<br>';
+            echo '- Montaż armatury i zabezpieczeń<br>';
+            echo '- Instalacja wykonana w stali zaciskanej z izolacją kauczukową<br>';
+            echo '- Montaż filtra magnetycznego<br>';
+            echo '- Połączenie pompy ciepła z instalacja CO bezpośrednio<br>';
+            echo '- Wykonanie instalacji zasilającej i sterującej<br>';
+            echo '- Napełnienie układu oraz jego odpowietrzenie<br>';
+            echo '- Pierwsze uruchomienie w przypadku pompy ciepła Viessmann<br>';
+            echo '- Szkolenie w ramach obsługi urządzenia<br>';
+            echo '</p>';
+
+            echo '<h5>';
+            echo '<br>';
+            echo 'Dodatkowe opcje:<br>';
+            echo '</h5>';
+
+            echo '<p style="font-size:10px; line-height: 2;">';
+            echo '- Montaż zbiornika buforowego<br>';
+            echo '- Montaż grup pompowych oraz mieszających<br>';
+            echo '- Płukanie instalacji<br>';
             echo '</p>';
             }
+        echo '</div>';
         echo '</form>';
     }
 }
@@ -259,45 +301,20 @@ function cf_shortcode()
 {
     ob_start();
     $result = new Calculator();
-    if (isset($_POST['cf-count'])) {
-        $power = $result->calculate_power($_POST['cf-area'], $_POST['cf-standard']);
-        $pump_info = $result->get_pump_info($power);
-        $area = $_POST['cf-area'];
-        $standard = $_POST['cf-standard'];
+    $power = $result->calculate_power($_POST['cf-area'], $_POST['cf-standard']);
+    $pump_info = $result->get_pump_info($power);
+    $area = $_POST['cf-area'];
+    $standard = $_POST['cf-standard'];
 
-        if (is_array($pump_info) && !empty($pump_info)) {
-            $pump_info = reset($pump_info);
-            $name_of_pump = $pump_info["Nazwa"];
-            $id = $pump_info["Id"];
-            $efficiency = $pump_info["Moc"];
-            $price = $pump_info["Cena [PLN]"];
-            $link = $pump_info["Link"];
-        } else {
-            $name_of_pump = '';
-            $id = '';
-            $efficiency = '';
-            $price = '';
-            $link = '';
-        }
-
-        html_form_code($power, $area, $standard, $name_of_pump, $id, $efficiency, $price, $link);
+    if (isset($_POST['cf-count']) && !isset($_POST['cf-submitted'])) {
+        html_form_code($power, $area, $standard);
     } else if (isset($_POST['cf-submitted'])) {
-        $power = $result->calculate_power($_POST['cf-area'], $_POST['cf-standard']);
-        $area = $_POST['cf-area'];
-        $standard = $_POST['cf-standard'];
-        $name_of_pump = $_POST['cf-div-name'];
-        $id = $_POST['cf-div-id'];
-        $efficiency = $_POST['cf-div-efficieny'];
-        $price = $_POST['cf-div-price'];
-        $link = $_POST['cf-div-link'];
-        $result->deliver_mail($power, $area, $standard, $name_of_pump, $id, $efficiency, $price, $link);
-        html_results_code($power, $area, $standard, $name_of_pump, $id, $efficiency, $price, $link);
-    } else if (isset($_POST['cf-result'])) {
-        html_calculation_code();
+        $variable = $result->assign_variable($power, $area, $standard, $pump_info);
+        $result->send_mail($variable);
+        html_results_code($power, $area, $standard, $pump_info);
     } else {
         html_calculation_code();
     }
-
     return ob_get_clean();
 }
 
@@ -309,5 +326,5 @@ function onMailError($wp_error)
     echo "</pre>";
 }
 
-add_shortcode('power_calculator', 'cf_shortcode');
+add_shortcode('heat_pumps_power_calculator', 'cf_shortcode');
 ?>
